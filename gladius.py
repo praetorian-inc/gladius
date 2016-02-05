e81fe22e16d2c7daeded2ee4e847c3e285456076
thebarbershopper <cld251@gmail.com>
2016-02-05 08:55:33 -0600

    Print the current ruleset

--- gladius.py
+++ gladius.py
@@ -556,7 +556,7 @@ Add --hashcat to select the new binary.''', color='red')
     observer = Observer()
     observers = []
 
-    color("Current rule list: {}".format(args.ruleset), color='green', graphic='[-] ')
+    print(color("Current rule list: {}".format(args.ruleset), color='green', graphic='[-] '))
 
     for handler, path in handlers:
         if not os.path.exists(path):
